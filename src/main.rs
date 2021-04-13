4eadcadb2d3284d4cd286f3d47b72d4cd6e7e68d
Sergii <sergii.salata@gmail.com>
2021-04-13 10:53:01 +0200

    checking actix

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,10 @@ use actix::prelude::*;
 use std::borrow::Borrow;
 use std::time::Duration;
 
+const BATCH_SIZE: usize = 3;
+const BATCH_TIMEOUT: Duration = Duration::from_secs(1);
+
+
 struct PubSubEventA;
 struct PubSubEventB;
 
@@ -15,14 +19,16 @@ struct PubSubMessageBatch(Vec<PubSubMessage>);
 
 /// Actor that provides order shipped event subscriptions
 struct MessageSender {
-    buffer : Vec<PubSubMessage>,
-    target : Recipient<PubSubMessageBatch>
+    buffer: Vec<PubSubMessage>,
+    timeout_handler: Option<SpawnHandle>,
+    target: Recipient<PubSubMessageBatch>,
 }
 
 impl MessageSender {
     fn batch(&mut self) {
         let batch = self.buffer.drain(..).collect();
         self.target.borrow().do_send(PubSubMessageBatch(batch));
+        self.timeout_handler = None;
     }
 }
 
@@ -39,8 +45,23 @@ impl Handler<PubSubMessage> for MessageSender {
 
     fn handle(&mut self, msg: PubSubMessage, ctx: &mut Self::Context) {
         println!("Got message!");
+
         self.buffer.push(msg);
-        if self.buffer.len() >= 3 {
+
+        match self.timeout_handler {
+            Some(_) => (),
+            None => {
+                self.timeout_handler = Some(ctx.run_later(BATCH_TIMEOUT, |act, ctx| {
+                    act.batch();
+                }))
+            }
+        };
+
+        if self.buffer.len() >= BATCH_SIZE {
+            match self.timeout_handler {
+                None => true,
+                Some(handler) => ctx.cancel_future(handler),
+            };
             self.batch();
         }
     }
@@ -51,7 +72,7 @@ impl Actor for BatchMessageSenderActor {
     type Context = Context<Self>;
 
     fn started(&mut self, ctx: &mut Self::Context) {
-        ctx.set_mailbox_capacity(1);
+        ctx.set_mailbox_capacity(3);
     }
 }
 impl Handler<PubSubMessageBatch> for BatchMessageSenderActor {
@@ -65,9 +86,11 @@ impl Handler<PubSubMessageBatch> for BatchMessageSenderActor {
 async fn main() {
     let batch_agent = BatchMessageSenderActor.start();
     let sender = MessageSender {
-        buffer : Vec::new(),
-        target : batch_agent.recipient()
-    }.start();
+        buffer: Vec::new(),
+        target: batch_agent.recipient(),
+        timeout_handler: None,
+    }
+    .start();
     println!("started!");
 
     let _ = sender.do_send(PubSubMessage);
@@ -78,6 +101,5 @@ async fn main() {
     let _ = sender.do_send(PubSubMessage);
     let _ = sender.do_send(PubSubMessage);
 
-    tokio::time::sleep(Duration::from_secs(1)).await;
+    tokio::time::sleep(Duration::from_secs(5)).await;
 }
-
